dbaac6924c573622ba90492e2228bb2da4e97595
Csaba Kiss <csakis@lanl.gov>
2017-11-15 15:11:10 -0700

    first commit. Setting up marko with express

---
+++ app.js
@@ -0,0 +1,54 @@
+require('marko/node-require');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var markoExpress = require('marko/express');
+var template = require('./template/index.marko');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(markoExpress());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('lasso/middleware').serveStatic());
+app.get('/', function(req, res) {
+    res.marko(template, {name: "Csaba", viewEngine: "Marko with browser refresh", colors: ["red", "green", "blue", "brown", "purple"]});
+});
+
+//app.use('/', index);
+//pp.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
